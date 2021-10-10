da6fc930f7069cc43002e1634fd3516f68a1a721
Joshua876 <joshua.mcintosh@mymona.uwi.edu>
2021-10-10 17:42:44 -0500

    Winner Check

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,7 @@
 var board;
 var game_state = [];
 var stage = 1;
+
 window.onload = function () {
     board=document.getElementById("board");
     for (let i = 0; i < 9; i++) {
@@ -24,6 +25,7 @@ window.onload = function () {
                     stage=1;
                 }
             }
+            winstatus();
         });
         board.children.item(i).addEventListener("mouseover",function(){
             this.classList.add("hover");
@@ -32,4 +34,38 @@ window.onload = function () {
             this.classList.remove("hover");
         });
     }
+}
+
+function winstatus() {
+    if(game_state[0] == game_state[1] && game_state[0] == game_state[2] && game_state[0]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[0]+" is the Winner!";}
+    
+    if(game_state[3] == game_state[4] && game_state[3] == game_state[5] && game_state[3]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[3]+" is the Winner!";}
+
+    if(game_state[6] == game_state[7] && game_state[6] == game_state[8] && game_state[6]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[6]+" is the Winner!";}
+
+    if(game_state[0] == game_state[4] && game_state[0] == game_state[8] && game_state[0]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[0]+" is the Winner!";}
+    
+    if(game_state[2] == game_state[4] && game_state[2] == game_state[6] && game_state[2]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[2]+" is the Winner!";}
+        
+    if(game_state[0] == game_state[3] && game_state[0] == game_state[6] && game_state[0]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[0]+" is the Winner!";}
+
+    if(game_state[1] == game_state[4] && game_state[1] == game_state[7] && game_state[1]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[1]+" is the Winner!";}
+
+    if(game_state[2] == game_state[5] && game_state[2] == game_state[8] && game_state[2]){
+        document.getElementById("status").classList.add("you-won");
+        document.getElementById("status").textContent = "Congratulations! "+game_state[2]+" is the Winner!";}            
 }
